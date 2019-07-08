af15fc8ea5d44e66975480970b58e53b0972f3bc
Asker <au3wk4@gmail.com>
2019-07-08 23:13:32 +0800

    Flask and D3 for Fun

---
+++ nobel_viz.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 22:11:49 2019
+
+@author: asker
+"""
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+winners=[
+        {'name':'Albert Einstein','category':'Physics'},
+        {'name':'V.S. Naipaul','category':'Literature'},
+        {'name':'Dorothy Hodgkin','category':'Chemistry'}
+        ]
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/demolist")
+def demo_list():
+    return render_template('testj2.html',heading="A little winners' list",winners=winners)
+
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)
+    
